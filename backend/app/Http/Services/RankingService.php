350fd0835b10971e37d833ad9798e7dee054a9ec
Ederson Ferreira <ederson.dev@gmail.com>
2022-04-18 10:56:31 -0300

    adicionado logica para definição de ranking

--- backend/app/Http/Services/RankingService.php
+++ backend/app/Http/Services/RankingService.php
@@ -6,6 +6,10 @@ use App\Repository\RankingRepository;
 
 class RankingService extends Service
 {
+
+  protected $_listRecord = [];
+  protected $_rank = 0;
+
   public function __construct(RankingRepository $repository)
   {
     $this->repository = $repository;
@@ -17,6 +21,50 @@ class RankingService extends Service
    */
   public function index($request)
   {
-    return $this->repository->index($request)->get();
+    $collection = $this->repository->index($request)->get();
+
+    $collection->map(function($movement){
+      $movement->personalRecord->map(function($record){
+        $record->ranking = $this->getRanking($record);
+        return $record;
+      });
+      $this->resetVariables();
+      return $movement;
+    });
+    
+    return $collection;
+  }
+
+  /**
+   * @param int $recordValue
+   * @return void
+   */
+  public function storeValueRecord($recordValue)
+  {
+    if(!in_array($recordValue,$this->_listRecord,true)){
+      array_push($this->_listRecord,$recordValue);
+    }
+  }
+
+  /**
+   * @param App\Models\PersonalRecord $record
+   * @return int
+   */
+  public function getRanking($record)
+  {
+    if(!in_array($record->value,$this->_listRecord,true)){
+      $this->_rank++;
+    }
+    $this->storeValueRecord($record->value);
+    return $this->_rank;
+  }
+
+  /**
+   * @return void
+   */
+  public function resetVariables()
+  {
+    $this->_listRecord = [];
+    $this->_rank = 0;
   }
 }
